cebcc0ca282f6003a0f1ebef3b5fccad9bb4c8c1
RCnonS <ratchanon.somprasong@gmail.com>
2024-05-09 14:56:06 +0700

    build: created event condition on ex-1

--- ex-1.js
+++ ex-1.js
@@ -1,8 +1,8 @@
-let isOver18;
-let hasCriminalBlacklist;
+let isOver18 = 18;
+let hasCriminalBlacklist = !true;
 
 let isAllow;
 
 // Start coding here
-
+isAllow = isOver18 >= 18 && hasCriminalBlacklist == false;
 console.log(isAllow);
