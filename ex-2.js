2c4b82678a47bc9d241e406e3bed5292067331a5
RCnonS <ratchanon.somprasong@gmail.com>
2024-05-09 14:52:56 +0700

    build: created promotion condition on ex-2

--- ex-2.js
+++ ex-2.js
@@ -1,12 +1,19 @@
 // Exercise #2: Promotion Conditions
 
 // Start coding here
-let lastMonthPaidMoreThan4000;
-let isWeekday;
-let hasBoughtProductFromITCategory;
-let hasAttendedDiscountEvent;
-let isPlatinum;
+let lastMonthPaidMoreThan4000 = true;
+let isWeekday = true;
+let hasBoughtProductFromITCategory = false;
+let hasAttendedDiscountEvent = true;
+let isPlatinum = false;
 
 let hasPromotion;
+
+hasPromotion =
+  (lastMonthPaidMoreThan4000 == true &&
+    isWeekday == true &&
+    hasBoughtProductFromITCategory == false &&
+    hasAttendedDiscountEvent == false) ||
+  isPlatinum == true;
 
 console.log(hasPromotion);
