794f69bcf6859db47a2ca28c46d17dbee1643222
Apirak Fakin <apirakfakin@gmail.com>
2024-05-09 14:38:12 +0700

    feat: add ex-1

--- ex-1.js
+++ ex-1.js
@@ -3,6 +3,8 @@ let hasCriminalBlacklist;
 
 let isAllow;
 
-// Start coding here
+isOver18 = true;
+hasCriminalBlacklist = false;
+isAllow = isOver18 && !hasCriminalBlacklist
 
 console.log(isAllow);
